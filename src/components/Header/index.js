bf96219bbaf66dc29f53129ef5c79a23d10ce753
pilla13 <PolliSims@yandex.ru>
2023-10-07 19:03:59 +0300

    edit

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,7 +1,7 @@
 import logo from "../../components/Header/img/logo.svg";
 import arrow from "../../components/Header/img/arrow.svg";
 
-import "../../../src/styled/StyledHeader/index.css"
+import "../../../src/styled/StyledHeader/index.css";
 
 export default function Header() {
   return (
@@ -13,7 +13,39 @@ export default function Header() {
           </a>
 
           <nav className="styledNav">
-            
+            <div className="styledNav__gradient-left"></div>
+
+            <li className="styledNav__wrapper">
+              <a href="#" className="styledHav__a">
+                НАШИ ИГРЫ
+              </a>
+            </li>
+
+            <li className="styledNav__wrapper">
+              <a href="#" className="styledHav__a">
+                УСЛУГИ
+              </a>
+            </li>
+
+            <li className="styledNav__wrapper">
+              <a href="#" className="styledHav__a">
+                ИЗДАТЕЛЬСТВО
+              </a>
+            </li>
+
+            <li className="styledNav__wrapper">
+              <a href="#" className="styledHav__a">
+                КАРЬЕРА
+              </a>
+            </li>
+
+            <li className="styledNav__wrapper">
+              <a href="#" className="styledHav__a">
+                КОНТАКТЫ
+              </a>
+            </li>
+
+            <div className="styledNav__gradient-right"></div>
           </nav>
 
           <div className="headerLanguageButton">
